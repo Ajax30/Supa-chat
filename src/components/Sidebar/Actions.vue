a7c1e4b66504dd0a70a2e096cdcbbf31f4291399
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2026-04-02 12:03:21 +0300

    Make a component for My Profile action buttons (#8)

--- src/components/Sidebar/Actions.vue
+++ src/components/Sidebar/Actions.vue
@@ -1,43 +1,19 @@
 <template>
   <div class="d-flex my-actions">
-    <button class="text-white">
-      <i class="fa-solid fa-comment-dots"></i>
-      <div class="bubble"></div>
-    </button>
-    <button class="text-muted">
-      <i class="fa-solid fa-user-group"></i>
-    </button>
-    <button class="text-muted">
-      <i class="fa-solid fa-phone"></i>
-    </button>
-    <button class="text-muted">
-      <i class="fa-solid fa-gear"></i>
-    </button>
+    <SidebarButton :isActive="true" :hasBubble="true" icon="fa-comment-dots" />
+    <SidebarButton icon="fa-user-group" />
+    <SidebarButton icon="fa-phone" />
+    <SidebarButton icon="fa-gear" />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import SidebarButton from "@/components/Sidebar/SidebarButton.vue";
+</script>
 
 <style scoped>
 .my-actions {
   padding: 0 25px;
   justify-content: space-around;
-}
-
-.my-actions button {
-  position: relative;
-}
-
-.my-actions .bubble {
-  position: absolute;
-  width: 6px;
-  height: 6px;
-  border-radius: 50%;
-  top: 1px;
-  right: 0;
-  background-color: #44b6ff;
-  box-shadow:
-    0 0 6px #44b6ff,
-    0 0 12px #44b6ff;
 }
 </style>
