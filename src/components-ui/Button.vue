1d4fd0b55ba2c166a31e6f4d8b095005c74106d7
Razvan Zamfir <razvan_zamfir80@yahoo.com>
2026-04-20 17:13:27 +0300

    UI components (#11)
    * Form buttons as UI component
    
    * Form inputs as UI component

---
+++ src/components-ui/Button.vue
@@ -0,0 +1,64 @@
+<template>
+  <button :type="type" :class="[
+    colorClass,
+    { 'w-100': fullWidth }
+  ]" :disabled="disabled">
+    {{ text }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    default: ''
+  },
+  colorClass: {
+    type: String,
+    default: ''
+  },
+  type: {
+    type: String,
+    default: 'button'
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  fullWidth: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<style scoped>
+button {
+  line-height: 1;
+  outline: none;
+  border: 0;
+  border-radius: 3px;
+  padding: 10px 20px;
+  color: #fff;
+  font-size: 1rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+}
+
+.primary {
+  background: #372379;
+}
+
+.primary:hover {
+  background: #4e3e81;
+}
+
+.success {
+  background: #059441;
+}
+
+.success:hover {
+  background: #2ba45e;
+}
+</style>
---
+++ src/components-ui/Input.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="form-field">
+    <label class="d-block">{{ label }}</label>
+
+    <input class="d-block w-100" :type="type" :placeholder="placeholder" :value="modelValue" :disabled="disabled"
+      @input="$emit('update:modelValue', $event.target.value)" @blur="$emit('blur')" />
+
+    <span class="error" v-if="error">
+      {{ error }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: String,
+  type: {
+    type: String,
+    default: 'text'
+  },
+  placeholder: String,
+  modelValue: String,
+  error: String,
+  disabled: Boolean
+})
+
+defineEmits(['update:modelValue', 'blur'])
+</script>
+
+<style scoped>
+.form-field {
+  position: relative;
+  margin-bottom: 15px;
+}
+
+.error {
+  color: #dc3545;
+  font-size: 12px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+
+.form-field label {
+  padding-left: 6px;
+  color: #6c757d;
+  font-weight: bold;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"] {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  padding: 0 6px 3px 6px;
+  color: #8b8da9;
+  font-size: 14px;
+}
+</style>
